27a4769817d6475d2bf92bc9716c001ad7b11abe
AtharvRaoRege <atharvrege722@gmail.com>
2024-09-16 21:04:48 +0530

    tailwind done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return <div>
-    <h1>Hello from basic boiler plate!</h1>
+    <h1 className="text-red-700">Hello from basic boiler plate!</h1>
     <h1>{import.meta.env.VITE_API_KEY}</h1>
   </div>
 }
